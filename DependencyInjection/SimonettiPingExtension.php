690f315515912856781f4cee1c2ffae2448ff454
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2022-06-24 16:41:38 -0300

    feat: drop bellow 7.4, add support for symfony 6
    - removed support to php versions before 7.4
    - removed support to symfony before 4
    - removed symfony/debug dependence (abandoned)
    - add support to symfony 6 (symfony/lock has changed)
    - AbstractFileStorage is needed to keep symfony 4.0 support

--- DependencyInjection/SimonettiPingExtension.php
+++ DependencyInjection/SimonettiPingExtension.php
@@ -1,10 +1,12 @@
 <?php
 namespace Simonetti\PingBundle\DependencyInjection;
 
+use Simonetti\PingBundle\Lock\FileStorage;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Lock\Factory;
 
 /**
  * Class PingExtension
@@ -22,6 +24,14 @@ class SimonettiPingExtension extends Extension
 
         $definition = $container->getDefinition('simonetti.bundle.ping.lock');
         $definition->replaceArgument(0, $config['lock_name']);
+
+        if (class_exists(Factory::class)) {
+            $definition = $container->getDefinition('simonetti.bundle.ping.lock_factory');
+            $definition->setClass(Factory::class);
+
+            $definition = $container->getDefinition('simonetti.bundle.ping.lock_store');
+            $definition->setClass(FileStorage::class);
+        }
     }
 
 }
---
+++ Lock/AbstractFileStorage.php
@@ -0,0 +1,61 @@
+<?php
+namespace Simonetti\PingBundle\Lock;
+
+use Symfony\Component\Lock\BlockingStoreInterface;
+use Symfony\Component\Lock\Key;
+use Symfony\Component\Lock\PersistingStoreInterface;
+
+/**
+ * Class FileStorage
+ * @package Simonetti\PingBundle\Lock
+ */
+class AbstractFileStorage
+{
+    /**
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * FileStorage constructor.
+     * @param string $path
+     */
+    public function __construct(string $path = null)
+    {
+        if (!$path) {
+            $path = sys_get_temp_dir() . '/simonett-ping-bundle-lock-';
+        }
+
+        $this->path = $path;
+    }
+
+
+    public function save(Key $key)
+    {
+        file_put_contents($this->path . $key, 'lock');
+    }
+
+    public function waitAndSave(Key $key)
+    {
+        $this->save($key);
+    }
+
+    public function putOffExpiration(Key $key, $ttl)
+    {
+        throw new \Exception('');
+    }
+
+    public function delete(Key $key)
+    {
+        if (!$this->exists($key)){
+            return;
+        }
+
+        return unlink($this->path . $key);
+    }
+
+    public function exists(Key $key): bool
+    {
+        return file_exists($this->path . $key);
+    }
+}
